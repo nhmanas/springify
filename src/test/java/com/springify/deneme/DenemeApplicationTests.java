6990a034dfe88dab6928a544b2e2af88f7c234b4
ibrahim biten <ibrahimbiten@gmail.com>
2021-10-20 22:26:32 +0300

    initial commit

---
+++ src/test/java/com/springify/deneme/DenemeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springify.deneme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DenemeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
